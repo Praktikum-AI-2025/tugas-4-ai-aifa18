4cc70b5a36d97f1c2c4de053c315cbb1f94aa3c5
aifa18 <azminaifah2005@gmail.com>
2025-04-20 15:48:42 +0700

    selesai

--- Tugas 4.py
+++ Tugas 4.py
@@ -43,13 +43,66 @@ def solution_05():
 
     train_generator= ImageDataGenerator(rescale=1./255)
     
-    # YOUR CODE HERE
+    TRAINING_DIR = 'data/horse-or-human'
+    VALIDATION_DIR = 'data/validation-horse-or-human'
+
+    train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest'
+        )
+
+    validation_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+            TRAINING_DIR,
+            target_size=(150, 150),
+            batch_size=32,
+            class_mode='binary'
+        )
+
+    validation_generator = validation_datagen.flow_from_directory(
+            VALIDATION_DIR,
+            target_size=(150, 150),
+            batch_size=32,
+            class_mode='binary'
+        )
+
+    print("Data generator berhasil dibuat!")
+    print(f"Jumlah kelas training: {train_generator.num_classes}")
+    print(f"Contoh shape input: {train_generator.image_shape}")
 
     model=tf.keras.models.Sequential([
-        # YOUR CODE HERE, end with a Neuron Dense, activated by sigmoid
+            tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+            tf.keras.layers.MaxPooling2D(2, 2),
 
-                tf.keras.layers.Dense(1, activation='sigmoid') #DO NOT CHANGE THIS LINE!
+            tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2,2),
+
+            tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2,2),
+
+            tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Flatten(),
+
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(1, activation='sigmoid') #DO NOT CHANGE THIS LINE!
         ])
+    
+    model.compile(
+    optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+    )
+
+    model.summary()
 
     return model
 
